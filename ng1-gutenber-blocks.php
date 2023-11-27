9820ac1f0659e974f89269573e7ad445a86f7c06
Nicolas <nicolas.gehin@pixelea.fr>
2023-11-27 11:36:20 +0100

    initial

---
+++ ng1-gutenber-blocks.php
@@ -0,0 +1,8 @@
+/**
+ * Plugin Name: ng1-gutenberg-block
+ * Plugin URI: https://github.com/ahmadawais/create-guten-block/
+ * Description: blocks Gutenberg by Ng1
+ * Author: GEHIN Nicolas
+ *
+ * @package CGB
+ */
